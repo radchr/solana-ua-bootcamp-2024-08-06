394157ff291d496a417b5ad6abd5b3cff3194eb0
Roman R <pochta.roman.r.git@gmail.com>
2024-08-11 21:51:19 +0300

    add rust pr 3

--- practice-1/rust-exerices/README.md
+++ practice-1/rust-exerices/README.md
@@ -1,1 +1,17 @@
-#Рішення практичного завдання 1 за допомогою Rust
+# Рішення практичного завдання 1 за допомогою Rust
+
+```rust
+use solana_sdk::signature::{Keypair, Signer};
+
+fn main() {
+    // Генерація нової пари ключів
+    let keypair = Keypair::new();
+
+    // Отримання публічного та приватного ключів
+    let public_key = keypair.pubkey();
+    let private_key = keypair.to_bytes();
+
+    println!("Публічний ключ: {}", public_key);
+    println!("Приватний ключ: {:?}", private_key);
+}
+```
---
+++ practice-3/rust-exerices/src/main.rs
@@ -0,0 +1,23 @@
+use solana_client::rpc_client::RpcClient;
+use solana_program::pubkey::Pubkey;
+use std::str::FromStr;
+
+fn main() {
+    // Підключення до devnet
+    let rpc_url = "https://api.devnet.solana.com";
+    let rpc_client = RpcClient::new(rpc_url.to_string());
+
+    // Ваша адреса (публічний ключ)
+    let address = "EH81TNMJE5yiivm4XfUqZpTQ8vVnhUbZd3Bfrocmdajc";
+    let pubkey = Pubkey::from_str(address).unwrap();
+
+    // Отримання балансу
+    match rpc_client.get_balance(&pubkey) {
+        Ok(balance) => println!(
+            "Баланс: {} лампортів, це {} SOL",
+            balance,
+            balance as f64 / 1000000000.0
+        ),
+        Err(err) => eprintln!("Помилка: {}", err),
+    }
+}
